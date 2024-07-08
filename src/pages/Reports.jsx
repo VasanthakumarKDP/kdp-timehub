07f14f73eceda8d805f23fbe4d10772c9d376cb9
VasanthakumarKDP <vkumar@kdpcorp.com>
2024-07-08 10:28:18 +0530

    updated code

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,40 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Select from "react-select";
+import React from "react";
 
-const MultiSelect = () => {
-  const [projects, setProjects] = useState([]);
-  const [selectedProjects, setSelectedProjects] = useState([]);
-
-  useEffect(() => {
-    fetch("https://localhost:7060/api/Project/GetallProject")
-      .then((response) => response.json())
-      .then((data) => {
-        // Assuming the data from the API is an array of objects
-        setProjects(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching projects:", error);
-      });
-  }, []); // Empty dependency array ensures the effect runs only once, after initial render
-
-  const handleChange = (selected) => {
-    setSelectedProjects(selected);
-  };
-
-  const parameterValue = "9497";
-  const urlWithParams = `https://kdpreporttesting.azurewebsites.net/mpreport?empid=${parameterValue}`;
-
-  return (
-    <>
-      <h1>Welcome to Notification</h1>
-      <iframe
-        src={urlWithParams}
-        style={{ width: "100%", height: "100" }}
-        className="iframe-no-scroll"
-        allowFullScreen
-      ></iframe>
-    </>
-  );
+const Reports = () => {
+  return <div>Reports</div>;
 };
 
-export default MultiSelect;
+export default Reports;
